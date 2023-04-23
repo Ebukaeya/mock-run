4655e917ca392219342a745dcaf82980f2551f11
Ebuka <eyaebuka@gmail.com>
2023-04-23 22:58:41 +0200

    lo

--- src/component/Narbar.jsx
+++ src/component/Narbar.jsx
@@ -8,7 +8,7 @@ const Narbar = () => {
       return(
             <>
          <div className="Narbar12">
-           AGC LOGO
+           <img style={{width:"30px"}} src="https://media.licdn.com/dms/image/C560BAQFM4e6JNU5zag/company-logo_200_200/0/1656697633107?e=2147483647&v=beta&t=3sm993uRUNvrBTrGPUgOUpd7VPhZUiMypHiR5SDnGAk"/>
          </div>
             </>
       )
